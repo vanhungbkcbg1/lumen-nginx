c1c35090dbf0e3d8cf694a1f32431134c09dbdf8
hungnv <vanhungbkcbg1@gmail.com>
2020-09-26 07:03:40 +0700

    authorization lumen

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,6 +9,12 @@ use Illuminate\Support\Facades\Gate;
 
 class PostController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware("auth");
+    }
+
     //
     public function store(Request $request){
         if(Gate::denies("posts.create")){
